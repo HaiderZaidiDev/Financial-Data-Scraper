f1ac2990d340c5c4a9d56856b631d8145bcbd0fe
Haider <haiderzaidi.schooL@gmail.com>
2020-08-08 23:01:56 -0400

    Added all ratios to scraper, all data is now scraped.

--- findata/pages/views.py
+++ findata/pages/views.py
@@ -11,46 +11,101 @@ import os
 import requests as r
 from bs4 import BeautifulSoup
 
+def infoScraper(ticker):
+    """ Scrapes company and price data from market watch"""
+    URL = 'https://www.marketwatch.com/investing/stock/{}/profile'.format(ticker)
+    page = r.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    nameResults = soup.find_all(id="instrumentname")
+    priceResults = soup.find_all(class_="pricewrap")
+    priceStatusResults = soup.find_all(class_="lastpricedetails")
+
+    companyInfo = {}
+    priceData = {} # Price is a seperate dictionary so we can track its currency.
+
+    for items in nameResults:
+        name = items.string
+        market = items.next_sibling.next_sibling.string
+        marketClean = market.replace('\r', '').replace('\n', '').strip()
+        companyInfo['name'] = name
+        companyInfo['market'] = marketClean
+
+    for items in priceResults:
+        if items.p['class'][0] == 'currency':
+            currency = items.p.string
+            price = items.p.next_sibling.next_sibling.string
+            priceData['currency'] = currency
+            priceData['price'] = price
+
+    for items in priceStatusResults:
+        points = items.span.string
+        percentage = items.span.next_sibling.next_sibling.string
+        priceStatus = '{0} {1}'.format(points, percentage)
+        priceData['priceStatus'] = priceStatus
+
+    companyInfo['priceData'] = priceData
+    return companyInfo
+
 
 def ratioScraper(ticker):
+    """ Scrapes financial ratios from Market Watch"""
     URL = 'https://www.marketwatch.com/investing/stock/{}/profile'.format(ticker)
     page = r.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
     results = soup.find_all(class_='section')
     ratios = {}
+    ratioPreSplit = {}
+    category = None
 
     for items in results:
         try:
             if items.p['class'][0] == 'column':
-                ratioValueObj = items.p.next_sibling.next_sibling
-                # The first sibling is whitespace, the 2nd one is the correct one.
-                ratioValues = ratioValueObj.string
+                # The current item is a section object, the oen above is whitespace,
+                # therefore the 2nd previous sibling is the header, which dictates
+                # the category for the raito e.g Valuation, Profitability
+                nestedSibling = items.previous_sibling.previous_sibling
+
+                if nestedSibling.name == 'h2': # The ratio categories are in h2 tags.
+                    category = nestedSibling.string
+                    ratioPreSplit = {} # Clears the previous category's ratios
+
+
                 ratioNames = items.p.string
-                djangoSafeRatioNames = ratioNames.replace(' ','_').replace('/', '_').replace('(', '').replace(')','')
-                # Django context variables don't work with spaces, so they must
-                # be removed. The / in some ratios also need to be removed as
-                # they create escape sequences.
-                ratios[djangoSafeRatioNames] = ratioValues
+                # Cuts loop once all ratios have been added to dict
+                if ratioNames == 'Industry':
+                    break
+                ratioValueObj = items.p.next_sibling.next_sibling
+                # The first sibling is whitespace, the 2nd one is the actual data.
+                ratioValues = ratioValueObj.string
+                ratioPreSplit[ratioNames] = ratioValues
+                ratios[category] = ratioPreSplit
 
         except: # Usually if Paragraph is NoneType
             pass
     return ratios
-
-
 
 def homeView(request):
     form = TickerForm(request.POST or None)
     if form.is_valid():
         form.save()
         tickerClean = form.cleaned_data['symbol']
+        ratios = ratioScraper(tickerClean)
+        companyInfo = infoScraper(tickerClean)
 
 
-
-
-        context = {'form': form}
-        context.update(ratioScraper(tickerClean))
-
-        print(context)
+        context = {
+        'form'              : form,
+        'name'              : companyInfo['name'],
+        'market'            : companyInfo['market'],
+        'currency'          : companyInfo['priceData']['currency'],
+        'price'             : companyInfo['priceData']['price'],
+        'priceStatus'       : companyInfo['priceData']['priceStatus'],
+        'valuation'         : ratios['Valuation'],
+        'efficiency'        : ratios['Efficiency'] ,
+        'liquidity'         : ratios['Liquidity'],
+        'profitability'     : ratios['Profitability'] ,
+        'capitalStructure'  : ratios['Capital Structure'],
+        }
 
         return render(request, 'ratios.html', context)
 
